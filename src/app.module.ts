642a56401b13e68eeb8cf24ae6077966b867be45
UrielRoblesG <urielrobles735@gmail.com>
2023-05-18 00:10:51 -0600

    correccion valores de variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,11 @@
 import { PatientStatusController } from './modules/admin/patient.status/patient.status.controller';
-import { PatientStatusService } from './modules/admin/patient.status/patient.status.service';
 import { PatientStatusModule } from './modules/admin/patient.status/patient.status.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from './database/entities/user.entity';
-import { CatUserType } from './database/entities/cat.user.type';
-import { PatientEntity } from './database/entities/patient.entity';
-import { DoctorEntity } from './database/entities/doctor.entity';
-import { CatRelationshipEntity } from './database/entities/cat.relationship.enity';
-import { CaregiverEntity } from './database/entities/caregiver.entity';
-import { CatPatientStatusEntity } from './database/entities/cat.patient.status.entity';
+
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './modules/auth/guard/auth.guard';
