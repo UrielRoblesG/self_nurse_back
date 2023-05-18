a62b5f06a0ceb6cce923c50155021e5fec075aa5
UrielRoblesG <urielrobles735@gmail.com>
2023-05-18 12:24:06 -0600

    Cambios para debug

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -8,18 +8,30 @@ export const DatabaseProvider: DynamicModule = TypeOrmModule.forRootAsync({
   inject: [ConfigService],
   async useFactory(config: ConfigService) {
     const isDevMode = config.get('NODE_ENV') !== Environment.Production;
+    console.log(
+      `Modo de desarrollo ${isDevMode ? 'Desarrollo' : 'Produccion'}`,
+    );
     const dbConfig = {
       type: 'mysql',
       host: config.get('DB_HOST'),
-      port: +config.get('DB_PORT'),
+      port: isDevMode ? +config.get('DB_PORT_DEV') : +config.get('DB_PORT'),
       username: config.get('DB_USER'),
       password: config.get('DB_PASSWORD'),
       database: config.get('DB_NAME'),
+      //   entities: [
+      //     UserEntity,
+      //     CatUserType,
+      //     PatientEntity,
+      //     DoctorEntity,
+      //     CaregiverEntity,
+      //     CatRelationshipEntity,
+      //     CatPatientStatusEntity,
+      //   ],
       synchronize: isDevMode,
       autoLoadEntities: true,
       logging: config.get('DB_LOGGING'),
     } as ConnectionOptions;
-
+    console.log(dbConfig);
     return dbConfig;
   },
 });
