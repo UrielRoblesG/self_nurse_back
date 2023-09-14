71550a4546c37a68ef507e91dab06b831a7b3502
Diego <diegoivan1987@outlook.com>
2023-09-13 22:55:54 -0600

    se corrigio el archivo main, el cliente ya recibe los eventos

--- apps/notification/src/main.ts
+++ apps/notification/src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { NotificationModule } from './notification.module';
-import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
   const socketApp = await NestFactory.create(NotificationModule);
-  socketApp.useWebSocketAdapter(new WsAdapter(socketApp));
   await socketApp.listen(3002);
 }
-
 bootstrap();
+
