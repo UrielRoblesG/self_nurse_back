95588e381d9b2c236bc0c2d1f22eeb54b6e1dfab
Diego <diegoivan1987@outlook.com>
2023-11-13 12:02:18 -0600

    se modifico la autenticacion y la ruta de los reportes

--- src/modules/paciente/vital-signs.controller.ts
+++ src/modules/paciente/vital-signs.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Body, Post } from '@nestjs/common';
+import { Controller, Body, Post,UseGuards } from '@nestjs/common';
 import { VitalSignsService } from './vital-signs.service';
+import { AuthGuard } from '../auth/guard/auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
-@Controller('vital-signs')
+@ApiBearerAuth()
+@UseGuards(AuthGuard)
+@Controller('api/vitalsigns/') 
 export class VitalSignsController {
   constructor(private readonly vitalSignsService: VitalSignsService) {}
   
-  @Post('api/vitalsigns/by-users')
+  @Post('by-users')
   async findByUserIds(@Body('userIds') userIds: number[]) {
     return await this.vitalSignsService.findByUserIds(userIds);
   }
--- src/modules/paciente/vital-signs.module.ts
+++ src/modules/paciente/vital-signs.module.ts
@@ -3,10 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { VitalSignsService } from './vital-signs.service';
 import { VitalSignsController } from './vital-signs.controller';
 import { VitalSignsEntity } from '../../database/entities/vital-signs.entity';
+import { JwtService } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([VitalSignsEntity])],
-  providers: [VitalSignsService],
+  imports: [TypeOrmModule.forFeature([VitalSignsEntity]),
+  PassportModule.register({ defaultStrategy: 'jwt' })],
+  providers: [VitalSignsService, JwtService],
   controllers: [VitalSignsController],
 })
 export class VitalSignsModule {}
